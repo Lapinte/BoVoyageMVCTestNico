c94f460ab80f96b596b7726f980552242c81846e
admin <admin@admin.com>
2018-07-20 09:36:14 +0200

    Ajout de nugget Jquery Validate et TiniMCE

--- BoVoyageMVCTestNico/BoVoyageMVCTestNico/Models/Destination.cs
+++ BoVoyageMVCTestNico/BoVoyageMVCTestNico/Models/Destination.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BoVoyageMVCTestNico.Models
 {
@@ -23,7 +24,9 @@ namespace BoVoyageMVCTestNico.Models
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Le champ {0} doit contenir entre {2} et {1} caractères")]
         public string Region { get; set; }
 
-        [Display(Name = "Description :")]
+        [AllowHtml]
+        [Display(Name = "Description")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
     }
 }
