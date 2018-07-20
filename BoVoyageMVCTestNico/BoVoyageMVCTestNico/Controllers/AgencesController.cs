c87a71ad39aa71b12448258267b956afaed35dd0
Lapinte <nicolas.lapinte@gmail.com>
2018-07-20 08:07:24 +0200

    Ajout controleur et vue Agence

---
+++ BoVoyageMVCTestNico/BoVoyageMVCTestNico/Controllers/AgencesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BoVoyageMVCTestNico.Data;
+using BoVoyageMVCTestNico.Models;
+
+namespace BoVoyageMVCTestNico.Controllers
+{
+    public class AgencesController : Controller
+    {
+        private BoVoyageMVCTestNicoDbContext db = new BoVoyageMVCTestNicoDbContext();
+
+        // GET: Agences
+        public ActionResult Index()
+        {
+            return View(db.Agences.ToList());
+        }
+
+        // GET: Agences/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agence agence = db.Agences.Find(id);
+            if (agence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agence);
+        }
+
+        // GET: Agences/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agences/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nom,CreatedAt,Deleted,DeletedAt")] Agence agence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agences.Add(agence);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agence);
+        }
+
+        // GET: Agences/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agence agence = db.Agences.Find(id);
+            if (agence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agence);
+        }
+
+        // POST: Agences/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nom,CreatedAt,Deleted,DeletedAt")] Agence agence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agence).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agence);
+        }
+
+        // GET: Agences/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agence agence = db.Agences.Find(id);
+            if (agence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agence);
+        }
+
+        // POST: Agences/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agence agence = db.Agences.Find(id);
+            db.Agences.Remove(agence);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
